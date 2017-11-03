4ae38dd0ead6155bc6ac0214372ded95adc1118f
Mostafa Abobakr <mostafaabobakr777@gmail.com>
2017-11-03 17:17:43 +0200

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,23 @@
+/*global $,*/
+$(function () {
+    'use strict';
+    $('html').niceScroll();
+    // slider height:
+    var winH = $(window).height(),
+        upperH = $('.upperbar').innerHeight(),
+        navH = $('.navbar').innerHeight();  
+    $('.slider, .carousel-item').height(winH - (upperH + navH));
+    // features work:
+    $('.featured-work ul li').on('click', function (){
+        $(this).addClass('active').siblings().removeClass('active');
+        if($(this).data('class') === "all"){
+            $('.shuffle-imgs .col-md ').css('opacity',1)
+        }
+        else{
+            $('.shuffle-imgs .col-md ').css('opacity',0.2)
+            $($(this).data('class')).parent().css('opacity',1) 
+        }
+    });
+
+
+});
